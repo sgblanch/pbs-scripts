2530498f22d1e01d07f968ba669c9fec8c19d391
Steven Blanchard <sgblanch@uncc.edu>
2019-03-11 12:51:33 -0400

    add hpcusage script

---
+++ hpcusage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Convience script to report cluster usage on a torque-based cluster.
+
+qstat -at | awk '
+	$10=="R" {
+		job[$2]++
+		jobc++
+		cpu[$2] += $7
+		cpuc += $7
+	} $10=="Q" {
+		queue[$2]++
+		queuec++
+		qcpu[$2] += $7
+		qcpuc += $7
+	} END {
+		PROCINFO["sorted_in"] = "@val_num_desc"
+		print "User    \tJobs\tCPUs\tQueue\tQueued CPUs"
+		print "--------\t----\t----\t-----\t-----------"
+		for (user in cpu) {
+			printf("%-8s\t%4g\t%4g\t%5g\t%11g\n", user, job[user], cpu[user], queue[user], qcpu[user])
+			delete queue[user]
+		}
+		for (user in queue) {
+			printf("%-8s\t%4g\t%4g\t%5g\t%11g\n", user, 0, 0, queue[user], qcpu[user])
+		}
+		print "        \t====\t====\t=====\t==========="
+		printf("%-8s\t%4g\t%4g\t%5g\t%11g\n", "", jobc, cpuc, queuec, qcpuc)
+	}'
